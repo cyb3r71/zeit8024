a1d714c23a6dbf870ad7da7b1aac1053d55f68cf
cyb3r71 <63894419+cyb3r71@users.noreply.github.com>
2022-06-12 22:13:38 +0200

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class Hello {
   public static void main(String[] args) {
   // displays "Hello Everyone" in console
-  System.out.printIn("Hello Everyone from Poland");
+  System.out.println("Hello Everyone from Poland");
   }
 }
